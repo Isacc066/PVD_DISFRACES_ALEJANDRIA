1ea5a0dded9076487109f46100354358d9e1f97b
Isacc066 <s23120207@alumnos.itsur.edu.mx>
2026-03-11 21:45:39 -0600

    Clases y Formularios agregados en sus respectivas capas(carpetas)

---
+++ PVD_DISFRACES_ALEJANDRIA/Modelos/ReporteVentaPorEmpleado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PVD_DISFRACES_ALEJANDRIA.Modelos
+{
+    internal class ReporteVentaPorEmpleado
+    {
+    }
+}
